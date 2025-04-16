411685c3f035b76cb7e6108471ed190b0701afe2
Mariani Dorado <nanidorado17@gmail.com>
2025-04-16 11:59:59 -0500

    iniciando entorno

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT= process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Hola Mundo</h1>
+        <h2>Hola Mundo desde Express v2</h2>
+        <p>Esta es una aplicacion de prueba</p>`);
+});
+
+app.listen(PORT, () => {
+    console.log(`la aplicacion esta corriendo en el puerto ${PORT}`);
+});
